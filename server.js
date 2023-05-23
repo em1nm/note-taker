3d8dbaaa75d9810073c84741899dff49d125bbca
em1nm <em.martirosyan@gmail.com>
2023-05-23 12:07:42 -0700

    index, notes html; db json, middleware and uuid

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-// Potential use of uuid (to be determined later)
+// use of uuid 
 const uuid = require('./helpers/uuid');
 
 
